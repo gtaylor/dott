51adb5a95417c5b053dced956e583b2458068df6
Greg Taylor <gtaylor@duointeractive.com>
2010-11-02 23:22:45 -0400

    Misc. cleanup with pylint in the mix.

--- src/server/server.py
+++ src/server/server.py
@@ -11,26 +11,26 @@ from mongomud import settings
 from mongomud.src.server.protocols.telnet import MudTelnetProtocol
 from mongomud.src.server.session_manager import SessionManager
 
-class MongoMudService(service.Service):
+class MudService(service.Service):
     """
     The main server service task.
-    """    
+    """
     def __init__(self):
         # Holds the TCP services.
         self.service_collection = None
         self.game_running = True
-            
+
         # Begin startup debug output.
-        print('\n' + '-'*50)         
+        print('\n' + '-' * 50)
 
         self.start_time = time.time()
 
         # Make output to the terminal. 
         print(' %s started on port(s):' % settings.GAME_NAME)
         for port in settings.LISTEN_PORTS:
-            print('  * %s' % port)              
+            print('  * %s' % port)
         print('-'*50)
-           
+
     def shutdown(self, message=None):
         """
         Gracefully disconnect everyone and kill the reactor.
@@ -39,9 +39,9 @@ class MongoMudService(service.Service):
             message = 'The server has been shutdown. Please check back soon.'
         SessionManager.announce_all(message)
         SessionManager.disconnect_all_sessions()
-        reactor.callLater(0, reactor.stop)
-        
-    def getMongoMudServiceFactory(self):
+        reactor.callLater(0, reactor.stop) #@UndefinedVariable
+
+    def get_mud_service_factory(self):
         """
         Retrieve instances of the server
         """
@@ -50,19 +50,19 @@ class MongoMudService(service.Service):
         factory.server = self
         return factory
 
-    def start_services(self, application):
+    def start_services(self, app_to_start):
         """
         Starts all of the TCP services.
         """
-        self.service_collection = service.IServiceCollection(application)
+        self.service_collection = service.IServiceCollection(app_to_start)
         for port in settings.LISTEN_PORTS:
-            mongomud_server = \
-                internet.TCPServer(port, self.getMongoMudServiceFactory())
-            mongomud_server.setName('mongomud%s' %port)
+            factory = self.get_mud_service_factory()
+            mongomud_server = internet.TCPServer(port, factory) #@UndefinedVariable
+            mongomud_server.setName('mongomud%s' % port)
             mongomud_server.setServiceParent(self.service_collection)
 
 # Twisted requires us to define an 'application' attribute.
-application = service.Application('mongomud') 
+application = service.Application('mongomud')
 # The main mud service. Import this for access to the server methods.
-mud_service = MongoMudService()
+mud_service = MudService()
 mud_service.start_services(application)
