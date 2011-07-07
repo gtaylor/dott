5703265959e5adc97c6d454f06a2210250fda149
Greg Taylor <gtaylor@duointeractive.com>
2011-07-06 21:57:48 -0400

    Adding a stub class for player objects.

---
+++ src/game/parents/base_objects/player.py
@@ -0,0 +1,8 @@
+from src.game.parents.base_objects.base import BaseObject
+
+class PlayerObject(BaseObject):
+    """
+    All players inherit this parent class. It further extends BaseObject with
+    player-specific behavior.
+    """
+    pass
