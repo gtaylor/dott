0ccefb15e2ca781c72f40b1a2357866a8fa6680b
Greg Taylor <gtaylor@duointeractive.com>
2011-07-07 22:17:58 -0400

    Fix failing unit tests.

--- src/server/accounts/__init__.py
+++ src/server/accounts/__init__.py
@@ -1,5 +1,5 @@
-from src.server.accounts.db_store import DBAccountStore
+from src.server.accounts.in_memory_store import InMemoryAccountStore
 
 # Go through this object to get/retrieve accounts values. Never go directly
 # through CouchDB.
-ACCOUNT_STORE = DBAccountStore()
+ACCOUNT_STORE = InMemoryAccountStore()
