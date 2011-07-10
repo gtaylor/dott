a5f80caa168a1f1b954ca9bfc5b4f3db4436e5a6
Greg Taylor <gtaylor@duointeractive.com>
2011-07-10 01:20:35 -0400

    Add a ThingObject, make PlayerObject inherit it.

--- src/game/parents/base_objects/player.py
+++ src/game/parents/base_objects/player.py
@@ -1,8 +1,8 @@
-from src.game.parents.base_objects.base import BaseObject
+from src.game.parents.base_objects.thing import ThingObject
 
-class PlayerObject(BaseObject):
+class PlayerObject(ThingObject):
     """
-    All players inherit this parent class. It further extends BaseObject with
+    All players inherit this parent class. It further extends ThingObject with
     player-specific behavior.
     """
     pass
---
+++ src/game/parents/base_objects/thing.py
@@ -0,0 +1,17 @@
+from src.game.parents.base_objects.base import BaseObject
+
+class ThingObject(BaseObject):
+    """
+    'Thing' is somewhat of a generic term, but I'm a loss for what else to
+    call it. A thing is anything that can be physically touched, picked up,
+    or interacted with. These can be stuff like an ExitObject
+    (sub-classes ThingObject), or a weapon, a vehicle, or even a
+    PlayerObject (sub-classes ThingObject).
+
+    It is perfectly acceptable to use this class directly if your Thing has no
+    special behaviors.
+
+    Unlike a RoomObject, ThingObject has a location, and may be picked up.
+    However, a ThingObject can carry other ThingObjects, like a RoomObject.
+    """
+    pass
