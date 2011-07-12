4048b031e8e64df008300e405470f6f80256d3f3
Greg Taylor <gtaylor@duointeractive.com>
2011-07-11 21:36:20 -0400

    Yank IDLE_TIMEOUT config. We'll worry about that later.

--- src/server/config/defaults.py
+++ src/server/config/defaults.py
@@ -3,6 +3,6 @@ Default config values. If no value for a config parameter can be found
 in the config DB in in_memory_store, these values are pulled as defaults.
 """
 DEFAULTS = {
-    'IDLE_TIMEOUT': 3600,
+    # This will be set at initial DB creation.
     'NEW_PLAYER_ROOM': None,
 }
