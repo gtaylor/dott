4d9a7952e0696182f30f18ce553dcd69ac04adb7
Greg Taylor <gtaylor@duointeractive.com>
2011-07-19 18:38:39 -0400

    Beginning to switch how some of the MudService's core components are initialized.

--- src/game/commands/general.py
+++ src/game/commands/general.py
@@ -6,3 +6,9 @@ class CmdLook(BaseCommand):
 
     def func(self, invoker, parsed_cmd):
         invoker.emit_to('WHO YOU LOOKIN AT?')
+
+class CmdWho(BaseCommand):
+    name = 'who'
+
+    def func(self, invoker, parsed_cmd):
+        invoker.emit_to('WHO YOU LOOKIN AT?')
--- src/utils/test_utils.py
+++ src/utils/test_utils.py
@@ -6,26 +6,11 @@ from src.server.config.in_memory_store import InMemoryConfigStore
 from src.server.objects.in_memory_store import InMemoryObjectStore
 from src.server.sessions.session_manager import SessionManager
 
-class DottTestCase(unittest2.TestCase):
+class MockMudService(object):
     """
-    Some helpers for unit testing.
+    Mocks up the MudService class found in dott.tac.
     """
-    def setUp(self):
-        """
-        By default, create a bare minimal set of data stores.
-        """
-        self.create_clean_game_env()
-
-    def tearDown(self):
-        """
-        Delete the created data stores between unit tests.
-        """
-        self.cleanup_game_env()
-
-    def create_clean_game_env(self):
-        """
-        Creates a fresh set of stores, DBs, and etc.
-        """
+    def __init__(self):
         self.global_cmd_table = GlobalCommandTable()
         self.command_handler = CommandHandler(
             command_table=self.global_cmd_table,
@@ -47,6 +32,34 @@ class DottTestCase(unittest2.TestCase):
             object_store=self.object_store)
         # Can't do recursive initialization, so do this after the fact.
         self.object_store._account_store = self.account_store
+
+class DottTestCase(unittest2.TestCase):
+    """
+    Some helpers for unit testing.
+    """
+    def setUp(self):
+        """
+        By default, create a bare minimal set of data stores.
+        """
+        self.create_clean_game_env()
+
+    def tearDown(self):
+        """
+        Delete the created data stores between unit tests.
+        """
+        self.cleanup_game_env()
+
+    def create_clean_game_env(self):
+        """
+        Creates a fresh set of stores, DBs, and etc.
+        """
+        self.mud_service = MockMudService()
+        self.global_cmd_table = self.mud_service.global_cmd_table
+        self.command_handler = self.mud_service.command_handler
+        self.config_store = self.mud_service.config_store
+        self.session_manager = self.mud_service.session_manager
+        self.object_store = self.mud_service.object_store
+        self.account_store = self.mud_service.account_store
 
     def cleanup_game_env(self):
         """
