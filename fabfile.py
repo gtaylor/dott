436f9a6bc9ec79e72305d28ead967db5fe1b6096
Greg Taylor <gtaylor@duointeractive.com>
2011-10-24 00:35:43 -0400

    Adding a 'get_db' fab command to pull the latest DB.

--- fabfile.py
+++ fabfile.py
@@ -4,5 +4,19 @@ from fabric.api import *
 env.hosts = ['dott@gc-taylor.com']
 
 def deploy():
+    """
+    Pulls the latest changes on the production server. For the time being,
+    you'll still need to @restart in-game to apply changes. In the future,
+    we'll just: kill -s HUP <pid>.
+    """
     with cd('/home/dott/dott'):
         run('git pull')
+
+def get_db():
+    """
+    Pulls the latest DB from production.
+    """
+    local('mkdir -p db')
+    get('/var/lib/couchdb/dott_accounts.couch', local_path='db')
+    get('/var/lib/couchdb/dott_objects.couch', local_path='db')
+
