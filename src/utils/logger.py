cdbbe03c9fcc6f83ee734c46024272723582b3b1
Greg Taylor <gtaylor@l11solutions.com>
2013-01-24 23:13:14 -0500

    PEP8'ification of logger module.

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,22 +5,24 @@ This file should have an absolute minimum in imports. If you'd like to layer
 additional functionality on top of some of the methods below, wrap them in
 a higher layer module.
 """
+
 from traceback import format_exc
 
 from twisted.python import log
 
 from src.utils.general import to_str
 
+
 def trace(errmsg=None):
     """
-    Log a traceback to the log. This should be called
-    from within an exception. errmsg is optional and
-    adds an extra line with added info. 
+    Log a traceback to the log. This should be called from within an exception.
+    errmsg is optional and adds an extra line with added info.
     """
+
     tracestring = format_exc()
     if tracestring:
         for line in tracestring.splitlines():
-            log.msg('[::] %s' % line)    
+            log.msg('[::] %s' % line)
     if errmsg:
         try:
             errmsg = to_str(errmsg)
@@ -28,7 +30,8 @@ def trace(errmsg=None):
             errmsg = str(e)
         for line in errmsg.splitlines():
             log.msg('[EE] %s' % line)
-            
+
+
 def error(errmsg):
     """
     Prints/logs an error message to the server log.
@@ -36,19 +39,21 @@ def error(errmsg):
     errormsg: (string) The message to be logged.
     """
     try:
-        errmsg = to_str(errmsg)    
+        errmsg = to_str(errmsg)
     except Exception, e:
         errmsg = str(e)
     for line in errmsg.splitlines():
         log.msg('[EE] %s' % line)
     #log.err('ERROR: %s' % (errormsg,))
 
+
 def warning(warnmsg):
     """
     Prints/logs any warnings that aren't critical but should be noted.
-    
+
     warnmsg: (string) The message to be logged.
     """
+
     try:
         warnmsg = to_str(warnmsg)
     except Exception, e:
@@ -57,12 +62,14 @@ def warning(warnmsg):
         log.msg('[WW] %s' % line)
     #log.msg('WARNING: %s' % (warnmsg,))
 
+
 def info(infomsg):
     """
     Prints any generic debugging/informative info that should appear in the log.
 
     infomsg: (string) The message to be logged.
     """
+
     try:
         infomsg = to_str(infomsg)
     except Exception, e:
